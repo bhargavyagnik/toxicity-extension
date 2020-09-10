e4c448927e048e7881acb723a2e77e2a72ebba5b
bhargavyagnik <41851612+bhargavyagnik@users.noreply.github.com>
2020-09-10 23:39:59 +0530

    updates

--- content.js
+++ content.js
@@ -2,26 +2,5 @@ chrome.runtime.onMessage.addListener(function (request) {
     //alert(request)
     var dc = document.documentElement.innerHTML
     var sp = dc.split(".")
-    var tjs = document.createElement('script');
-    tjs.setAttribute('src',"https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest")
-    document.head.appendChild(tjs);
-    var tox = document.createElement('script');
-    tox.setAttribute('src',"https://cdn.jsdelivr.net/npm/@tensorflow-models/toxicity")
-    var counter = 0
-    document.head.appendChild(tox);
-    const threshold = 0.9;
-    toxicity.load(threshold).then(model => {
-        for (index = 0; index < sp.length; index++) {
-            model.classify(sp).then(predictions => {
-            //console.log(predictions);
-            for(i=0; i<7; i++){
-                if(predictions[i].results[0].match){
-                    counter++;
-                    
-                    }  
-                }
-            });
-        }
-    })
-    alert(counter)
+    sendResponse({sent: sp})
 })
--- popup.js
+++ popup.js
@@ -6,4 +6,28 @@ document.addEventListener('DOMContentLoaded', function () {
             chrome.tabs.sendMessage(tabs[0].id, 'hi')
         })
 }
+    function setCount(res){
+        const div = document.createElement('div')
+        div.textContent = {}
+    }
 }, false)
+
+
+
+//
+//var counter = 0
+//    const threshold = 0.9;
+//    toxicity.load(threshold).then(model => {
+//        for (index = 0; index < sp.length; index++) {
+//            model.classify(sp).then(predictions => {
+//            //console.log(predictions);
+//            for(i=0; i<7; i++){
+//                if(predictions[i].results[0].match){
+//                    counter++;
+//                    
+//                    }  
+//                }
+//            });
+//        }
+//    })
+//import * as toxicity from '@tensorflow-models/toxicity';
